c9fee2fdb1edc16592dcbe5130747361108f14f7
Samiron Paul <samironpaul@gmail.com>
2018-06-08 22:26:40 -0300

    Added a merge sorter using inplace merging. N^2 complexity.

--- src/main/java/org/samiron/drilling/Sortings.java
+++ src/main/java/org/samiron/drilling/Sortings.java
@@ -5,9 +5,15 @@ import org.samiron.mylibs.MergeSorter;
 public class Sortings {
 
     public static void main(String[] args) {
-        Integer[] unsortedNumbers = {2, 3, 5, 7, 1, 9, 10, -1, 0, 200};
-        MergeSorter<Integer> mSorter = new MergeSorter<>(unsortedNumbers);
-        mSorter.sort();
-        mSorter.show();
+        Integer[] unsortedNumbers = {3, 2, 5, 7, 1, 9, 10, -1, 0, 200};
+        MergeSorter<Integer> numSorter= new MergeSorter<>(unsortedNumbers);
+        numSorter.sort();
+        numSorter.show();
+
+        String[] unsortedNames = {"Samiron", "jolly", "Shreyasi", "Srijon"};
+        MergeSorter<String> nameSorter = new MergeSorter<>(unsortedNames);
+        nameSorter.sort();
+        nameSorter.show();
+
     }
 }
--- src/main/java/org/samiron/mylibs/MergeSorter.java
+++ src/main/java/org/samiron/mylibs/MergeSorter.java
@@ -1,22 +1,61 @@
 package org.samiron.mylibs;
 
-import java.util.Collection;
-
-public class MergeSorter<T> {
+public class MergeSorter<T extends Comparable> {
 
     private T[] items;
-    public MergeSorter(T[] items){
+
+    public MergeSorter(T[] items) {
         this.items = items;
     }
 
-    public void show(){
-        for(T i : items){
+    public void show() {
+        for (T i : items) {
             System.out.println(i);
         }
     }
 
     public void sort() {
+        mergeSort(0, items.length - 1);
+    }
 
+    private void mergeSort(int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int midpoint = (start + end) / 2;
+        mergeSort(start, midpoint);
+        mergeSort(midpoint + 1, end);
+        merge(start, end, midpoint);
+    }
+
+    private void merge(int start, int end, int midpoint) {
+        int i = start;
+        int j = midpoint + 1;
+        while (i <= midpoint) {
+            if (this.items[i].compareTo(this.items[j]) > 0) {
+                swap(i, j);
+                push(j, end);
+            }
+            i++;
+        }
+    }
+
+    private void push(int start, int end) {
+        while (start < end) {
+            if(this.items[start].compareTo(this.items[start+1]) > 0){
+                swap(start, start + 1);
+                start ++;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void swap(int i, int j) {
+        T temp = this.items[i];
+        this.items[i] = this.items[j];
+        this.items[j] = temp;
     }
 
     /**
